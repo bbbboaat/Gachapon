9abb24073c8c5d8e02c4da405c98751144e6ecc4
Nanthawat <boat@Nanthawats-MacBook-Air.local>
2022-05-18 18:46:40 +0700

    add database

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require ('express');
+const { MongoClient } = require('mongodb');
 const app = express ();
 const PORT = 3000;
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb+srv://boat:<password>@cluster0.0pjc1.mongodb.net/test',()=>{
+    console.log("connect database")
+})
 
 app.get('/',function(req,res){
     res.send("Hello World");
