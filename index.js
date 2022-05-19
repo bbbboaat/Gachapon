5447beb8f0316ff1e74b3d6886d98204e2a656af
Nanthawat <boat@Nanthawats-MacBook-Air.local>
2022-05-19 14:04:21 +0700

    login

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require("express");
-const parser = require ("body-parser")
+const parser = require("body-parser");
 const mongoose = require("mongoose");
 const app = express();
 const PORT = 3000;
-const User = require('./mongoose/user');
+const User = require("./mongoose/user");
+const tokenModel = require("./mongoose/user");
 const bodyParser = require("body-parser");
+
+const rand = function() {
+    return Math.random().toString(36).substr(2);
+
+};
+
+const tokenGen = function () {
+    return rand() + rand ();
+}
 
 app.use(bodyParser.json());
 
@@ -20,8 +30,8 @@ app.get("/", function (req, res) {
 });
 
 app.post("/register", async (req, res) => {
-    console.log("success")
-    
+  console.log("success");
+
   let resStatus = 200;
   let resMessage = "Success";
 
@@ -29,30 +39,52 @@ app.post("/register", async (req, res) => {
   let _password = req.body.password || "";
   let _role = req.body.role || "";
   let _tier = req.body.tier || "";
-  
-  if(_username === "" || _password === "" || _role === "" || _tier === ""){
-      resStatus = 400;
-      resMessage = "bad request";
-      res.status(resStatus).send({ resMessage });
+
+  if (_username === "" || _password === "" || _role === "" || _tier === "") {
+    resStatus = 400;
+    resMessage = "bad request";
+    res.status(resStatus).send({ resMessage });
   }
   let newUser = User({
-      username:_username,
-      password:_password,
-      role:_role,
-      tier:_tier
+    username: _username,
+    password: _password,
+    role: _role,
+    tier: _tier,
   });
+  await newUser.save()
 
-  try {
-      await newUser.save();
-  }
-  catch (e){
-      console.log(e)
-  }
-  console.log(`${_username}`)
   res.status(resStatus).send({ resMessage });
-  
 });
 
+app.post("/login", async (req, res) => {
+  let resStatus = 200;
+  let resMessage = "Success";
+  let _username = req.body.username || "";
+  let _password = req.body.password || "";
+
+  let usernameid = await User.findOne({
+    username: _username,
+    password: _password
+  });
+
+  console.log(`username : ${_username} , password : ${_password}`)
+  if (usernameid === null) {
+    console.log("NULL!!");
+    let resStatus = 400;
+    let resMessage = "Login Failed";
+    res.status(resStatus).send({ resMessage });
+  }else {
+      console.log(usernameid);
+    
+      await tokenModel.deleteMany({ userId: usernameid._id });
+      let newToken = tokenModel({
+        tokenString: tokenGen(),
+        userId: usernameid._id,
+      });
+      await newToken.save();
+  }
+
+});
 app.listen(PORT, () => {
   console.log(`server is listening on port ${PORT}`);
 });
